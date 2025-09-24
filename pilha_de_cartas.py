c2b1ba82a6c4073fdca31a988afe762304292626
Daniel Baumann <143436969+danielbaumannbr@users.noreply.github.com>
2025-09-24 15:38:17 -0300

    Add stack operations for card pile management
    Introduced functions to handle stack operations: empilhar, desempilha, esta_vazia, topo, and tamanho. These functions support managing piles of cards for gameplay. Also added variables for monte_descarte and mao_jogador to simulate card piles.

--- pilha_de_cartas.py
+++ pilha_de_cartas.py
@@ -10,6 +10,24 @@ def criar_baralho():
     #embaralhar
     random.shuffle(baralho)
     return baralho
+
+def empilhar(pilha,carta):
+    pilha.append(carta)
+
+def desempilha(pilha):
+    if esta_vazia(pilha):
+        raise IndexError("Não é possivel remover da pilha vazia.")
+    return pilha.pop()
+def esta_vazia(pilha):
+    return len(pilha)==0
+def topo(pilha):
+    return None if esta_vazia(pilha) else pilha[-1]
+def tamanho(pilha):
+    return len(pilha)
 # simulação
 baralho = criar_baralho()
-print(baralho)
+#print(baralho)
+monte_descarte=[]
+#lista 
+mao_jogador=[]
+
